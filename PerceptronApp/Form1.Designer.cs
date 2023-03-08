1eaec606ae03ded0899281367b5922f1c0d2a95c
Charlette-Tulod <111828267+Charlette-Tulod@users.noreply.github.com>
2023-03-08 23:11:23 +0800

    Add files via upload

---
+++ PerceptronApp/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿using System.Drawing;
+
+namespace PerceptronApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput1 = new System.Windows.Forms.TextBox();
+            this.txtInput2 = new System.Windows.Forms.TextBox();
+            this.btnPredict = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtLearningRate = new System.Windows.Forms.TextBox();
+            this.txtNumIterations = new System.Windows.Forms.TextBox();
+            this.btnTrain = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.reset1 = new System.Windows.Forms.Button();
+            this.reset2 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput1
+            // 
+            this.txtInput1.Location = new System.Drawing.Point(605, 248);
+            this.txtInput1.Name = "txtInput1";
+            this.txtInput1.Size = new System.Drawing.Size(111, 20);
+            this.txtInput1.TabIndex = 0;
+            // 
+            // txtInput2
+            // 
+            this.txtInput2.Location = new System.Drawing.Point(605, 308);
+            this.txtInput2.Name = "txtInput2";
+            this.txtInput2.Size = new System.Drawing.Size(111, 20);
+            this.txtInput2.TabIndex = 1;
+            // 
+            // btnPredict
+            // 
+            this.btnPredict.Location = new System.Drawing.Point(544, 358);
+            this.btnPredict.Name = "btnPredict";
+            this.btnPredict.Size = new System.Drawing.Size(75, 23);
+            this.btnPredict.TabIndex = 2;
+            this.btnPredict.Text = "Predict";
+            this.btnPredict.UseVisualStyleBackColor = true;
+            this.btnPredict.Click += new System.EventHandler(this.btnPredict_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(602, 406);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(314, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 37);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Perceptron";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift SemiLight Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(346, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "AND Logic Gate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(541, 249);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Input 1:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(541, 312);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Input 2:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(541, 404);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Result:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(134, 292);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Learning Rate:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(134, 349);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 16);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Iterations:";
+            // 
+            // txtLearningRate
+            // 
+            this.txtLearningRate.Location = new System.Drawing.Point(234, 289);
+            this.txtLearningRate.Name = "txtLearningRate";
+            this.txtLearningRate.Size = new System.Drawing.Size(100, 20);
+            this.txtLearningRate.TabIndex = 11;
+            // 
+            // txtNumIterations
+            // 
+            this.txtNumIterations.Location = new System.Drawing.Point(234, 348);
+            this.txtNumIterations.Name = "txtNumIterations";
+            this.txtNumIterations.Size = new System.Drawing.Size(100, 20);
+            this.txtNumIterations.TabIndex = 12;
+            // 
+            // btnTrain
+            // 
+            this.btnTrain.Location = new System.Drawing.Point(137, 398);
+            this.btnTrain.Name = "btnTrain";
+            this.btnTrain.Size = new System.Drawing.Size(75, 23);
+            this.btnTrain.TabIndex = 13;
+            this.btnTrain.Text = "Train";
+            this.btnTrain.UseVisualStyleBackColor = true;
+            this.btnTrain.Click += new System.EventHandler(this.btnTrain_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(143, 442);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 15);
+            this.label8.TabIndex = 14;
+            // 
+            // reset1
+            // 
+            this.reset1.ForeColor = System.Drawing.Color.Red;
+            this.reset1.Location = new System.Drawing.Point(259, 398);
+            this.reset1.Name = "reset1";
+            this.reset1.Size = new System.Drawing.Size(75, 23);
+            this.reset1.TabIndex = 15;
+            this.reset1.Text = "Reset";
+            this.reset1.UseVisualStyleBackColor = true;
+            this.reset1.Click += new System.EventHandler(this.reset1_Click);
+            // 
+            // reset2
+            // 
+            this.reset2.ForeColor = System.Drawing.Color.Red;
+            this.reset2.Location = new System.Drawing.Point(641, 358);
+            this.reset2.Name = "reset2";
+            this.reset2.Size = new System.Drawing.Size(75, 23);
+            this.reset2.TabIndex = 16;
+            this.reset2.Text = "Reset";
+            this.reset2.UseVisualStyleBackColor = true;
+            this.reset2.Click += new System.EventHandler(this.reset2_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(84, 165);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(175, 19);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Training the Perceptron";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(85, 210);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(198, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Default values are with closest accuracy";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(85, 239);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(329, 13);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "Note: Changing the Learning Rate and Iterations will affect the result";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(538, 163);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(39, 19);
+            this.label12.TabIndex = 20;
+            this.label12.Text = "Test";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(541, 208);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(96, 13);
+            this.label13.TabIndex = 21;
+            this.label13.Text = "Enter values 0 or 1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FloralWhite;
+            this.ClientSize = new System.Drawing.Size(841, 526);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.reset2);
+            this.Controls.Add(this.reset1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnTrain);
+            this.Controls.Add(this.txtNumIterations);
+            this.Controls.Add(this.txtLearningRate);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnPredict);
+            this.Controls.Add(this.txtInput2);
+            this.Controls.Add(this.txtInput1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput1;
+        private System.Windows.Forms.TextBox txtInput2;
+        private System.Windows.Forms.Button btnPredict;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtLearningRate;
+        private System.Windows.Forms.TextBox txtNumIterations;
+        private System.Windows.Forms.Button btnTrain;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button reset1;
+        private System.Windows.Forms.Button reset2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ PerceptronApp/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PerceptronApp
+{
+    public partial class Form1 : Form
+    {
+        private Perceptron perceptron;
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Initialize the perceptron with 2 input neurons
+            perceptron = new Perceptron(2);
+
+            // Will be enable after training
+            btnPredict.Enabled = false;
+            reset2.Enabled = false;
+
+
+            // Set default values for the learning rate and number of iterations
+            txtLearningRate.Text = "0.1";
+            txtNumIterations.Text = "1000";
+
+        }
+
+        private void btnPredict_Click(object sender, EventArgs e)
+        {
+            // Get the input values from the text boxes
+            double[] inputs = new double[2];
+            inputs[0] = Convert.ToDouble(txtInput1.Text);
+            inputs[1] = Convert.ToDouble(txtInput2.Text);
+
+            // Use the perceptron to predict the output value
+            double output = perceptron.Predict(inputs);
+
+            // Display the output value in the result label
+            lblResult.Text = output.ToString();
+        }
+
+        private void btnTrain_Click(object sender, EventArgs e)
+        {
+            // Get the input values from the text boxes
+            double[] inputs1 = new double[] { 0, 0 };
+            double[] inputs2 = new double[] { 0, 1 };
+            double[] inputs3 = new double[] { 1, 0 };
+            double[] inputs4 = new double[] { 1, 1 };
+
+            double[] outputs = new double[] { 0, 0, 0, 1 };
+
+            // Get the learning rate and number of iterations from the text boxes
+            double learningRate = Convert.ToDouble(txtLearningRate.Text);
+            int numIterations = Convert.ToInt32(txtNumIterations.Text);
+
+            // Train the perceptron with the given inputs and outputs, learning rate, and number of iterations
+            perceptron.Train(new double[][] { inputs1, inputs2, inputs3, inputs4 }, outputs, learningRate, numIterations);
+
+            // Enable the predict button after training
+            btnPredict.Enabled = true;
+            reset2.Enabled = true;
+
+            label8.Text = "Trained!";
+        }
+
+        private void reset2_Click(object sender, EventArgs e)
+        {
+            txtInput1.Clear();
+            txtInput2.Clear();
+        }
+
+        private void reset1_Click(object sender, EventArgs e)
+        {
+            txtLearningRate.Clear();
+            txtNumIterations.Clear();
+        }
+    }
+
+    public class Perceptron
+    {
+        private double[] weights;
+        private double bias;
+
+        public Perceptron(int inputSize)
+        {
+            weights = new double[inputSize];
+            for (int i = 0; i < inputSize; i++)
+            {
+                weights[i] = 0;
+            }
+            bias = 0;
+        }
+
+        public double Predict(double[] inputs)
+        {
+            double weightedSum = 0;
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                weightedSum += inputs[i] * weights[i];
+            }
+            weightedSum += bias;
+            return Activate(weightedSum);
+        }
+
+        public void Train(double[][] inputs, double[] outputs, double learningRate, int numIterations)
+        {
+            for (int iteration = 0; iteration < numIterations; iteration++)
+            {
+                for (int i = 0; i < inputs.Length; i++)
+                {
+                    double prediction = Predict(inputs[i]);
+                    double error = outputs[i] - prediction;
+
+                    for (int j = 0; j < weights.Length; j++)
+                    {
+                        weights[j] += learningRate * error * inputs[i][j];
+                    }
+
+                    bias += learningRate * error;
+                }
+            }
+        }
+
+        private double Activate(double weightedSum)
+        {
+            if (weightedSum > 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
